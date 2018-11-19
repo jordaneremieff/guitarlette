813406adb2cfa652ed4f45c7398d93a7b28d4b16
Jordan <jordan@eremieff.com>
2018-11-20 00:00:42 +1100

    Parser

--- guitarlette/parser.py
+++ guitarlette/parser.py
@@ -1,6 +1,7 @@
 from pychord import Chord
 from dataclasses import dataclass
 from typing import List
+from functools import partial
 
 CONTENT = """
 Am D C Fm
@@ -19,9 +20,17 @@ class SongRowItem:
     content: str
     chord: Chord = None
 
-    def transpose(self, n):
-        if self.chord is not None:
+    def format_html(self):
+        if self.is_chord:
+            self.content = f"<span style='font-weight:bold;'>{self.content}</span>"
+
+    def transpose(self, n: int) -> None:
+        if self.is_chord:
             self.chord.transpose(n)
+
+    @property
+    def is_chord(self) -> bool:
+        return self.chord is not None
 
 
 @dataclass
@@ -29,14 +38,20 @@ class SongContent:
 
     rows: List[List]
 
-    @property
-    def get_fingerings(self):
-        return self._get_fingerings
-
-    def transpose(self, n):
+    def _apply_method(self, method: str, *args, **kwargs) -> None:
         for row in self.rows:
             for item in row:
-                item.transpose(n)
+                getattr(item, method)(*args, **kwargs)
+
+    def format_html(self) -> None:
+        self._apply_method("format_html")
+
+    def transpose(self, n: int) -> None:
+        self._apply_method("transpose", n)
+
+    # @property
+    # def get_fingerings(self):
+    #     return self._get_fingerings
 
 
 def parse_chord(element):
@@ -56,9 +71,14 @@ def song_parser(raw_data):
     raw_data = [i for i in raw_data.strip().split("\n") if i]
     rows = [parse_row(row) for row in raw_data]
     content = SongContent(rows=rows)
-    print(content)
-    content.transpose(3)
-    print(content)
+    return content
 
 
-song_parser(CONTENT)
+song = song_parser(CONTENT)
+
+song.transpose(3)
+print(song)
+song.transpose(5)
+print(song)
+song.format_html()
+print(song)
